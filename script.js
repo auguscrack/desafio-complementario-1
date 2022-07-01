f14dda352bfd8b72592e0791a77a21682be2cad9
auguscrack <augus40@yahoo.com>
2022-06-30 21:18:20 -0300

    todo terminado

---
+++ script.js
@@ -0,0 +1,49 @@
+while (confirm("¿Desea convertir un número de español a ingles?")) {
+    let numeroEspañol = prompt("INGRESE UN NUMERO ESCRITO EN ESPAÑOL (DEL 1 AL 20)").toLowerCase()
+    if (numeroEspañol == "uno") {
+        alert("su numero traducido a ingles es= one");
+    }else if (numeroEspañol == "dos") {
+        alert("su numero traducido a ingles es= two");
+    }else if (numeroEspañol == "tres") {
+        alert("su numero traducido a ingles es= three");
+    }else if (numeroEspañol == "cuatro") {
+        alert("su numero traducido a ingles es= four");
+    }else if (numeroEspañol == "cinco") {
+        alert("su numero traducido a ingles es= five");
+    }else if (numeroEspañol == "seis") {
+        alert("su numero traducido a ingles es= six");
+    }else if (numeroEspañol == "siete") {
+        alert("su numero traducido a ingles es= seven");
+    }else if (numeroEspañol == "ocho") {
+        alert("su numero traducido a ingles es= eight");
+    }else if (numeroEspañol == "nueve") {
+        alert("su numero traducido a ingles es= nine");
+    }else if (numeroEspañol == "diez") {
+        alert("su numero traducido a ingles es= ten");
+    }else if (numeroEspañol == "once") {
+        alert("su numero traducido a ingles es= eleven");
+    }else if (numeroEspañol == "doce") {
+        alert("su numero traducido a ingles es= twelve");
+    }else if (numeroEspañol == "trece") {
+        alert("su numero traducido a ingles es= thirteen");
+    }else if (numeroEspañol == "catorce") {
+        alert("su numero traducido a ingles es= fourteen");
+    }else if (numeroEspañol == "quince") {
+        alert("su numero traducido a ingles es= fifteen");
+    }else if (numeroEspañol == "dieciseis") {
+        alert("su numero traducido a ingles es= sixteen");
+    }else if (numeroEspañol == "diecisiete") {
+        alert("su numero traducido a ingles es= seventeen");
+    }else if (numeroEspañol == "dieciocho") {
+        alert("su numero traducido a ingles es= eighteen");
+    }else if (numeroEspañol == "diecinueve") {
+        alert("su numero traducido a ingles es= nineteen");
+    }else if (numeroEspañol == "veinte") {
+        alert("su numero traducido a ingles es= twenty");
+    }
+    else {
+        alert("NÚMERO EN ESPAÑOL EQUIVOCADO, INGRESE DE NUEVO"); 
+    }
+    console.log(numeroEspañol)
+    
+    }
